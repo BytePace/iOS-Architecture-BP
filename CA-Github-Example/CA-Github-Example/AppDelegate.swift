cfa3e77f2f6220744cf25c3996b904c2d331007e
Nikita Velichkin <velichkinns@gmail.com>
2020-02-17 23:46:24 +0600

    Init example project

---
+++ CA-Github-Example/CA-Github-Example/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CA-Github-Example
+//
+//  Created by Nikita Velichkin on 17/02/2020.
+//  Copyright © 2020 Nikita Velichkin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
